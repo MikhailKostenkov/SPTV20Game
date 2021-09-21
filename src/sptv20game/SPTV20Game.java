53a013749552cde311f865d969926dc63aee383f
Darja Kuznetsova <darja.kuznetsova@ivkhk.ee>
2021-09-21 14:48:29 +0300

    Game2

--- src/sptv20game/SPTV20Game.java
+++ src/sptv20game/SPTV20Game.java
@@ -21,16 +21,24 @@ public class SPTV20Game {
         //программа загадыввает число и просит пользователя отгадать
         //Если пользователь отгадал, он выйграл
         //Иначе пользователь проиграл
-        int max = 5,min = 0;
+        int max = 5,min = 0, attempt = 1;
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int gameNum = random.nextInt(max - min + 1) + min;
         System.out.println("Программа загадала число от 0 до 5, угадай: ");
-        Scanner scanner = new Scanner(System.in);
-        int userNum = scanner.nextInt();
-        if(gameNum == userNum)
-            System.out.println("Ура! ты Выйграл!");
-        else
-            System.out.println(":(Ты проиграл: Задумано число: " + gameNum);
+        for(int i=0;i<3; i++){
+            int userNum = scanner.nextInt();
+            if(gameNum == userNum){
+                System.out.println("Ура! Ты выйграл!");
+               break;
+           }else{
+                if(attempt < 3){
+                    System.out.println("Неправильно, попробуй еще раз: ");
+            }else{
+                System.out.println(":(Ты проиграл: Задумано число: " + gameNum);
+            }
+        }
+        attempt++;    
     }
-    
+}
 }
